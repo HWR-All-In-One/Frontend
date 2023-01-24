c1fa03692d10b6b0c9c7e2ee37670989d8dff364
Saeed Albaradie <saeed.albaradie@mitel.com>
2023-01-24 15:16:40 +0100

    Tag-Monat-Jahr Icon.Logo,Footer, background edited

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,12 @@
       fixed
       app
     >
-      <div class="logo">
-          <img src="../assets/v.png" alt="HWR-APP-logo">
+      
+      <v-list>  
+        <div class="logo">
+          <img src="../assets/HWR-lOGO-3.png" alt="HWR-APP-logo">
         </div>
-      <v-list>        
+
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -21,28 +23,66 @@
             <v-icon >{{ item.icon }}</v-icon>
           </v-list-item-action>
         </v-list-item>
+
       </v-list>
+
     </v-navigation-drawer>
+
     <v-app-bar
       fixed
       app
     >
       <v-toolbar-title
-      permanent
-      fixed
-      width:15rem
       >{{ title }}
       </v-toolbar-title>
     </v-app-bar>
+
     <v-main>
       <v-container>
         <DropdownList />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="fixed"
-      app
-    >
+
+    <v-row>
+      <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="button1"
+        @click="loader = 'loading3'"
+      >
+        Heute
+      </v-btn>
+      <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="button2"
+        @click="loader = 'loading3'"
+      >
+        Wochen
+      </v-btn>
+      <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="button3"
+        @click="loader = 'loading3'"
+      >
+        Monate
+      </v-btn>
+      <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="button4"
+        @click="loader = 'loading3'"
+      >
+        Jahr
+      </v-btn>
+    </v-row>
+  
+    <v-footer app bottom fixed style="width: max-content;" padless>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -73,11 +113,9 @@ export default {
           title: 'mail',
           to: '/mail'
         }
-        
       ],
       miniVariant: false,
       title: 'HWR AIO'
-
     }
   }
 }
@@ -87,9 +125,8 @@ export default {
   .v-navigation-drawer{
     display: flex;
     flex-direction: column;
-  
-    background-color: var(--dark);
-    color: var(--light);
+    block-size: max-content;
+    background-color: #000000;
 
     .logo {
       position: relative;
@@ -98,12 +135,39 @@ export default {
       background:rgba(255,255,255,0);
       opacity: 5;
       
-
       img {
-        height:2rem;
-        width:2rem;
+        height:3.5rem;
+        width:3.5rem;
       }
-      
     }
+  }
+
+  .theme--dark.v-footer {
+    background-color: #000000;
+  }
+  
+  .v-toolbar__title, .v-toolbar__content{
+    block-size: max-content;
+    background-color: #000000;
+  }
+
+ .row{
+  margin: auto 1rem auto auto;
+  flex: initial;
+  height: max-content;
+ }
+
+ .button1 {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 30px 60px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: auto 1rem 1rem auto;
+  cursor: pointer;
 }
+
 </style>
