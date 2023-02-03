abb4fb18fad6027d810ff48cffbb1f79faf45423
Saeed Albaradie <saeed.albaradie@mitel.com>
2023-02-03 12:42:03 +0100

    inspire deleted and inserted old default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,13 @@
-s<template>
-  <v-app>
+<template>
+  <v-app dark>
     <v-navigation-drawer
-      permanent
-      mini-variant
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
       fixed
       app
     >
-      
-      <v-list>  
-        <div class="logo">
-          <img src="../assets/HWR-AIO-lOGO.svg" alt="HWR-APP-logo">
-        </div>
-
+      <v-list>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -20,23 +16,40 @@ s<template>
           exact
         >
           <v-list-item-action>
-            <v-icon >{{ item.icon }}</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-
       </v-list>
-
     </v-navigation-drawer>
-      
     <v-main>
       <v-container>
-        <DropdownList />
+        <Nuxt />
       </v-container>
     </v-main>
-  
-
-  
-    <v-footer app bottom fixed style="width: max-content;" padless>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -44,99 +57,24 @@ s<template>
 
 <script>
 export default {
-  name: 'app',
-  
-  name: 'DefaultLayout',
-  data () {
-    return {
-      fixed: false,
-      
-      items: [
-        {
-          icon: 'mdi-account',
-          title: 'Account',
-          to: '/account'
-        },
-        {
-          icon: 'mdi-calendar-multiple',
-          title: 'calendar',
-          to: '../pages/calendar.vue'
-        }, 
-        {
-          icon: 'mdi-email',
-          title: 'mail',
-          to: '/mail'
-        }
-      ],
-      miniVariant: false,
-      title: 'HWR AIO'
+    name: "DefaultLayout",
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            items: [
+                {
+                    icon: "mdi-apps",
+                    title: "Welcome",
+                    to: "/"
+                }
+            ],
+            miniVariant: false,
+            right: true,
+            rightDrawer: false,
+            title: "HWR All-In-One"
+        };
     }
-  }
 }
-</script> 
-
-<style lang="scss">
-
-.theme--dark.v-app-bar.v-toolbar.v-sheet {
-    background-color: transparent;
-}
-.theme--light.v-app-bar.v-toolbar.v-sheet {
-    background-color: transparent;
-    border:none;
-}
-  .theme--dark.v-navigation-drawer {
-    background-color: grey;
-    border: none;
-    
-
-    .logo {
-      position: relative;
-      margin-left:0.3rem;
-      
-      padding: auto;
-      background:rgba(255,255,255,0);
-      opacity: 6;
-      
-      img {
-        height:3rem;
-        width:3rem;
-      }
-    }
-  }
-
-  .theme--light.v-navigation-drawer {
-    background-color: transparent;
-    border: none;
-    
-
-    .logo {
-      position: relative;
-      margin-left:auto;
-      margin:0 0 0 1rem;padding: 0.5rem;
-      border-left: 1rem;
-      
-      opacity: 5;
-      
-      img {
-        height:3rem;
-        width:3rem;
-      }
-    }
-  }
-
-  .theme--dark.v-footer {
-    background-color: transparent;
-  }
-
-  .theme--light.v-footer {
-    background-color: transparent;
-  }
-  
-  .v-toolbar__title, .v-toolbar__content{
-    block-size: max-content;
-    
-  }
-
-
-
-</style>
+</script>
