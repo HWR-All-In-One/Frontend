aae8361eb093c904b3d37bb2140b4e16e76bdeca
Saeed Albaradie <saeed.albaradie@mitel.com>
2023-02-04 06:11:40 +0100

    inserting changing into default

--- layouts/default.vue
+++ layouts/default.vue
@@ -45,7 +45,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer :absolute="!fixed" app style="width: max-content;">
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
